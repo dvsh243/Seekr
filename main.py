f58a7988dd37598abeb34e978e8827e71b8306fe
devesh <92008218+dvsh243@users.noreply.github.com>
2023-04-03 23:21:27 +0530

    performance timing

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import time
 
 seekr = Seekr('data/companies.sqlite')
 
-start_time = time.perf_counter()
-
-
 while True:
     print()
     target = input("select term to search : ")
+    
+    start_time = time.perf_counter()
     matches = seekr.get_matches(target, 3)
-
     print(f"fetched results in {str(time.perf_counter() - start_time)[:5]} seconds.", end='\n\n')
 
     for match in matches:
