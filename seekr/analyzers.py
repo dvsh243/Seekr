0d2df14dc64e10d9394a0d322f8b8b677fbf44ff
shimmer4 <ahujadevesh1@gmail.com>
2023-04-06 11:30:16 +0530

    more analyzers?

--- seekr/analyzers.py
+++ seekr/analyzers.py
@@ -1,11 +1,9 @@
 
-
-
 def whitespace(document: str) -> list:
     return document.split(' ')
 
 
-def ngrams(document: str, n: int = 3) -> list:
+def ngrams(document: str) -> list:
     ngram_list = []
     for i in range(2, len(document)):
         ngram_list.append( document[i - 2: i + 1] )
