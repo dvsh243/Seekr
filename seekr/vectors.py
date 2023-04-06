304eb52a7d920e223cb8356112b364afe28eb949
shimmer4 <ahujadevesh1@gmail.com>
2023-04-06 11:34:40 +0530

    .

--- seekr/vectors.py
+++ seekr/vectors.py
@@ -4,17 +4,13 @@ class Vector:
 
     def __init__(self, array: list) -> None:
         self.raw_array = array
-
         self.featureMap = {idx: tfidf for idx, tfidf in array}
-        # print(self.featureMap)
-
     
     def magnitude(self) -> float:
         mag = 0
         for idx, tfidf in self.raw_array:
             mag += math.pow(tfidf, 2)
         return math.sqrt(mag)
-
 
     def __repr__(self) -> str:
         return f"<Vector: {self.raw_array}>"
