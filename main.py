8916486af6c9558b925c7254dc899d500bc06747
shimmer4 <ahujadevesh1@gmail.com>
2023-04-10 21:24:57 +0530

    matches working

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import time
 seekr = Seekr()
 seekr.load_from_db(location = 'data/companies.sqlite', column = 1)
 
-seekr.get_matches('holdings inc')
 
-# while True:
-#     print()
-#     target = input("select term to search : ")
+while True:
+    print()
+    target = input("select term to search : ")
     
-#     start_time = time.perf_counter()
-#     matches = seekr.get_matches(target, 3)
-#     print(f"fetched {len(matches)} results in {str(time.perf_counter() - start_time)[:5]} seconds.", end='\n\n')
+    start_time = time.perf_counter()
+    matches = seekr.get_matches(target, 3)
+    print(f"fetched {len(matches)} results in {str(time.perf_counter() - start_time)[:5]} seconds.", end='\n\n')
 
-#     for match in matches:
-#         print(str(match[1])[:5], match[0], sep='\t')
+    for match in matches:
+        print(str(match[0])[:5], match[1], sep='\t')
--- seekr/core.py
+++ seekr/core.py
@@ -16,8 +16,8 @@ class Seekr:
     def load_from_db(self, location: str, column: int) -> None:
         start_time = time.perf_counter()
         
-        db = DB(location, 20)
-        self.corpus = [cleanDocument(x[column]) for x in db.rows]
+        self.db = DB(location, 20)
+        self.corpus = [cleanDocument(x[column]) for x in self.db.rows]
 
         self.vectorize()
 
@@ -29,7 +29,7 @@ class Seekr:
         self.vectorizer = TfidfVectorizer()
         self.tfidf_matrix = self.vectorizer.fit_transform(
             corpus = self.corpus,
-            analyzer = whitespace
+            analyzer = ngrams
         )
 
     
@@ -41,4 +41,16 @@ class Seekr:
         target = cleanDocument(target)
         target_vector = self.vectorizer.doc_to_vector(target)
 
-        print(target_vector)
+        similarity = []  # min heap
+
+        for index, doc_vector in enumerate(self.vectorizer.matrix):
+            heapq.heappush(
+                similarity,
+                ( distance.euclidian_distance(target_vector, doc_vector), index )
+            )
+
+        res = []
+        for i in range(limit):
+            sim_value, index = heapq.heappop(similarity)
+            res.append( (sim_value, self.db.rows[index]) )
+        return res
