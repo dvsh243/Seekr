b6824fd922157fa174ea74e8435fb7764aabbdf1
shimmer4 <ahujadevesh1@gmail.com>
2023-04-12 23:04:02 +0530

    calculating actual euclidian distance

--- seekr/indexes/annoy.py
+++ seekr/indexes/annoy.py
@@ -8,14 +8,38 @@ with open('seekr/indexes/test_vectors.json') as f:
 
 
 
-def euclidian_distance(vector1, vector2) -> float:
-    dist = 0
-    doc_indexToValue = {index: value for index, value in vector2}
+def actual_euclidian_distance(vector1, vector2) -> float:
+    """
+    vector1 -> [(3, 0.53612), (7, 1.518630)]
+    vector2 -> [(0, 0.910361), (7, 2.11983), (9, 0.21591), (14, 1.85192)]
+    common = {0: (0, 0.910361), 3: (0.53612, 0), 7: (1.518630, 2.11983) ...}
+    """
+    vector1_map = {index: value for index, value in vector1}
+    vector2_map = {index: value for index, value in vector2}
+    common = {}
 
-    for index, value in vector1:
-        dist += math.pow(value - doc_indexToValue.get(index, 0), 2)
+    for index, value in vector1_map.items():
+        common[index] = (value, vector2_map.get(index, 0))
+
+    for index, value in vector2_map.items():
+        if index not in common:
+            common[index] = (0, value)
+    
+    dist = 0
+    for _, (value1, value2) in common.items():
+        dist += math.pow(value2 - value1, 2)
     
     return math.sqrt(dist)
+
+
+# def false_euclidian_distance(vector1, vector2) -> float:
+#         dist = 0
+#         doc_indexToValue = {index: value for index, value in vector2}
+
+#         for index, value in vector1:
+#             dist += math.pow(value - doc_indexToValue.get(index, 0), 2)
+        
+#         return math.sqrt(dist)
 
 
 def get_center(centers: list[list], vector: list):
@@ -23,7 +47,7 @@ def get_center(centers: list[list], vector: list):
     closer = (None, float('inf'))  # (center_index, distance) pair
     
     for center_index, center_vector in enumerate(centers):
-        distance = euclidian_distance(center_vector, vector)
+        distance = actual_euclidian_distance(center_vector, vector)
         if distance < closer[1]:
             closer = (center_index, distance)
     
@@ -46,10 +70,6 @@ for _ in range(20):
         center_count[center_index] += 1
 
     print(center_count)
-
-
-
-
 
 
 
