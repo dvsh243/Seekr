b5b253cdecc6178327d8d66a39659c361e9b77f7
Devesh Ahuja <ahujadevesh1@gmail.com>
2023-06-14 21:13:11 +0530

    menu driven `main.py`

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from seekr import Seekr
 import time
 
 seekr = Seekr()
-index_type = 'kmeans'
+index_type = input("-> enter an index type (linear/kmeans/annoy) : ")
+maxLimit = int( input("-> enter number of documents : ") )
 
-seekr.load_from_db('companies', 'data/companies.sqlite', column = 1, maxLimit = 10000)
+seekr.load_from_db('companies', 'data/companies.sqlite', column = 1, maxLimit = maxLimit)
 seekr.create_index(index_type)  # 'kmeans' or 'annoy'
 
 
 while True:
     print()
-    target = input("select term to search : ")
+    target = input("-> select term to search : ")
     
 
     print("\nBTree search :-")
     start_time = time.perf_counter()
-    matches = seekr.query(target, limit = 3, index_type = index_type)  # replace with 'kmeans' or 'annoy'
+    matches = seekr.query(target, limit = 3, index_type = index_type)
     print(f"fetched {len(matches)} results in {str(time.perf_counter() - start_time)[:5]} seconds.", end='\n\n')
     for match in matches: print(str(match[0])[:5], match[1], sep='\t')
 
